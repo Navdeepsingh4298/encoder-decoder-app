433753092728a79acdd44db09f4cc98d34ba0212
Navdeepsingh4298 <navdeepsingh4298@gmail.com>
2023-10-14 18:12:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import encryptDecryptKey from "./encryptDecryptKey";
+import "./App.css";
 
 function App() {
+  const [plainText, setPlainText] = useState("");
+  const [codedText, setCodedText] = useState("");
+
+  const magic = (e) => {
+    let action="";
+    let arrayOfWords=[]
+    let resultArray = [];
+    let tempResultArray = [];
+
+    (e.target.getAttribute("action") === "encrypt") ? action = "encrypt": action = "decrypt";
+    
+    (action === "encrypt") ? arrayOfWords = plainText.split(" ") : arrayOfWords = codedText.split(" ")
+      arrayOfWords.forEach((word) => {
+        let ArrayOfLetters = word.split("");
+        tempResultArray =[]
+        ArrayOfLetters.forEach((letter) => {
+          if(action === "encrypt"){
+          let resultObj = encryptDecryptKey.find(({plainLetter}) => plainLetter === letter)
+          resultObj ? tempResultArray.push(resultObj.codedLetter) : tempResultArray.push(letter)}
+          else if(action === "decrypt"){
+            let resultObj = encryptDecryptKey.find(({codedLetter}) => codedLetter === letter)
+          resultObj ? tempResultArray.push(resultObj.plainLetter) : tempResultArray.push(letter)
+          }
+        })
+        resultArray.push(tempResultArray.join(""))
+      });
+    (action === "encrypt") ? setCodedText(resultArray.join(" ")) : setPlainText(resultArray.join(" "))
+  
+  };
+
+  const handlePlainTextChange = (e) => {
+    e.preventDefault();
+    setPlainText(e.target.value);
+  };
+
+  const handleCodedTextChange = (e) => {
+    e.preventDefault();
+    setCodedText(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div id="encode-box">
+          <h2>Plain Text</h2>
+          <textarea
+            placeholder="type your plain text"
+            onChange={handlePlainTextChange}
+            name="encode-box-input"
+            id="encode-box-input"
+            value={plainText}
+          />
+          <button className="btn" type="submit" onClick={magic} action="encrypt">
+            Encrypt
+          </button>
+        </div>
+        <div id="decode-box">
+          <h2>Coded Text</h2>
+          <textarea
+            placeholder="type your coded text"
+            onChange={handleCodedTextChange}
+            name="encode-box-input"
+            id="decode-box-input"
+            value={codedText}
+          />
+          <button className="btn" type="submit" onClick={magic} action="decrypt">
+            Decrypt
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/encryptDecryptKey.js
@@ -0,0 +1,108 @@
+const encryptDecryptKey = [
+  {
+    plainLetter: "a",
+    codedLetter: "q",
+  },
+  {
+    plainLetter: "b",
+    codedLetter: "w",
+  },
+  {
+    plainLetter: "c",
+    codedLetter: "e",
+  },
+  {
+    plainLetter: "d",
+    codedLetter: "r",
+  },
+  {
+    plainLetter: "e",
+    codedLetter: "t",
+  },
+  {
+    plainLetter: "f",
+    codedLetter: "y",
+  },
+  {
+    plainLetter: "g",
+    codedLetter: "u",
+  },
+  {
+    plainLetter: "h",
+    codedLetter: "i",
+  },
+  {
+    plainLetter: "i",
+    codedLetter: "o",
+  },
+  {
+    plainLetter: "j",
+    codedLetter: "p",
+  },
+  {
+    plainLetter: "k",
+    codedLetter: "a",
+  },
+  {
+    plainLetter: "l",
+    codedLetter: "s",
+  },
+  {
+    plainLetter: "m",
+    codedLetter: "d",
+  },
+  {
+    plainLetter: "n",
+    codedLetter: "f",
+  },
+  {
+    plainLetter: "o",
+    codedLetter: "g",
+  },
+  {
+    plainLetter: "p",
+    codedLetter: "h",
+  },
+  {
+    plainLetter: "q",
+    codedLetter: "j",
+  },
+  {
+    plainLetter: "r",
+    codedLetter: "k",
+  },
+  {
+    plainLetter: "s",
+    codedLetter: "l",
+  },
+  {
+    plainLetter: "t",
+    codedLetter: "z",
+  },
+  {
+    plainLetter: "u",
+    codedLetter: "x",
+  },
+  {
+    plainLetter: "v",
+    codedLetter: "c",
+  },
+  {
+    plainLetter: "w",
+    codedLetter: "v",
+  },
+  {
+    plainLetter: "x",
+    codedLetter: "b",
+  },
+  {
+    plainLetter: "y",
+    codedLetter: "n",
+  },
+  {
+    plainLetter: "z",
+    codedLetter: "m",
+  },
+];
+
+export default encryptDecryptKey;
